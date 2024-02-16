1219e3f3788c1a00147e392bd9fa529e2816a0fa
cw <fromnowwon@gmail.com>
2024-02-16 21:26:24 +0900

    install socket.io

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+	res.sendFile(__dirname + "/index.html");
+});
 
 server.listen(4000, () => {
-  console.log('Now listening on port 4000')
-})
+	console.log("Now listening on port 4000");
+});

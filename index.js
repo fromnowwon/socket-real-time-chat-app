c8393d3ff28fde1a1bc9e7281a46a30c1d240b3c
cw <fromnowwon@gmail.com>
2024-02-20 19:56:32 +0900

    setting submit event

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ io.on("connection", (socket) => {
 	console.log("a user connected");
 	socket.on("disconnect", () => {
 		console.log("user disconnected");
-	})
+	});
 });
 
 server.listen(4000, () => {
